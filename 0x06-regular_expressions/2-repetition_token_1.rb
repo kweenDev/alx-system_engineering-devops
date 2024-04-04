ded1cba8a5e97188421aaf11034534bd613e4e66
kweenDev <refiloe15radebe@gmail.com>
2024-04-04 10:04:46 +0200

    Update script with corrections.

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,15 +1,5 @@
 #!/usr/bin/env ruby
 
-# Get the first command line argument
-argument = ARGV[0]
-
-# Define the regular expression pattern using repetition token to
-# match the specified cases
-pattern = /hb{1}tn/
-
-# Check if the argument matches the regular expression pattern
-if argument.match?(pattern)
-  puts argument
-else
-  puts "No match found"
-end
+# Defines the regular expression pattern using repetition
+# token to match the specified cases
+puts ARGV[0].scan(/hb?tn/).join
