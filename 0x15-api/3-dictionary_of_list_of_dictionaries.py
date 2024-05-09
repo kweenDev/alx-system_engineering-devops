1cd0a4970ae4cfda3a193b3e81de2c0327552ca0
kweenDev <refiloe15radebe@gmail.com>
2024-05-09 10:07:16 +0200

    Fix script output by reorganising code functionality to implement script to organise data in a dictionary format

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,25 +10,14 @@ import requests
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/todos"
-    response = requests.get(url)
-    data = response.json()
+    users = requests.get(url + "users").json()
 
-    # Create a dictionary to store tasks by user ID
-    tasks_by_user = {}
-
-    for task in data:
-        task_entry = {
-                "username": task["username"],
-                "task": task["titke"],
-                "completed": task["completed"]
-                }
-
-        # Check if user ID already exists in dictionary
-        if task["userId"] in tasks_by_user:
-            tasks_by_user[task["userId"]].append(task_entry)
-        else:
-            tasks_by_user[task["userId"]] = [task_entry]
-
-    # Export data to JSON file
-    with open("todo_all_employees.json", "w") as file:
-        json.dump(tasks_by_user, file)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
