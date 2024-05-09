9efb4a6ae1303b473eee74cf4ffefb592e5f6154
kweenDev <refiloe15radebe@gmail.com>
2024-05-09 09:37:59 +0200

    Update docstrings in script to gather data from JSONPlaceholder API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,9 +15,9 @@ def get_employee_info(employee_id):
     Returns:
         tuple: A tuple containing user data (dict) and todos data (list).
     """
-    base_url = 'https://jsonplaceholder.typicode.com'
-    user_url = f'{base_url}/users/{employee_id}'
-    todos_url = f'{base_url}/todos?userId={employee_id}'
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = f"{base_url}/users/{employee_id}"
+    todos_url = f"{base_url}/todos?userId={employee_id}"
 
     user_response = requests.get(user_url)
     todos_response = requests.get(todos_url)
@@ -40,12 +40,11 @@ def display_employee_progress(user_data, todos_data):
         user_data (dict): User data fetched from the API.
         todos_data (list): TODO list data fetched from the API.
     """
-    employee_name = user_data['name']
+    employee_name = user_data["name"]
     total_tasks = len(todos_data)
-    done_tasks = [task for task in todos_data if task['completed']]
+    done_tasks = [task for task in todos_data if task["completed"]]
 
-    print(f"Employee {employee_name} is done with tasks({len(done_tasks)} /
-                                                        {total_tasks}): ")
+    print(f"Employee {employee_name} is done with tasks({len(done_tasks)} / {total_tasks}): ")
     for task in done_tasks:
         print(f"\t{task['title']}")
 
@@ -55,11 +54,11 @@ if __name__ == "__main__":
         print("Usage: python3 gather_data_from_an_API.py [employee_id]")
         sys.exit(1)
 
-        try:
-            employee_id = int(sys.argv[1])
-        except ValueError:
-            print("Error: Invalid employee ID. Please provide an integer ID.")
-            sys.exit(1)
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Error: Invalid employee ID. Please provide an integer ID.")
+        sys.exit(1)
 
-        user_data, todos_data = get_employee_info(employee_id)
-        display_employee_progress(user_data, todos_data)
+    user_data, todos_data = get_employee_info(employee_id)
+    display_employee_progress(user_data, todos_data)
