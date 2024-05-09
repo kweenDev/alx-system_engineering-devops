57968d489d5cbd3bb6e84405d8e43efc5902c95d
kweenDev <refiloe15radebe@gmail.com>
2024-05-09 10:02:19 +0200

    Sort imported libraries into alphabetical order and fix code output to integrate script for exporting data to JSON format

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,70 +6,16 @@ import requests
 import sys
 
 
-def get_employee_info(employee_id):
-    """
-    Fetches employee information and TODO list from the API.
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    Args:
-        employee_id (int): The ID of the employee.
-
-    Returns:
-        tuple: A tuple containing user data (dict) and todos data (list).
-    """
-    base_url = 'https://jsonplaceholder.typicode.com'
-    user_url = f'{base_url}/users/{employee_id}'
-    todos_url = f'{base_url}/todos?userId={employee_id}'
-
-    user_respnse = requests.get(user_url)
-    todos_response = requests.get(todos_url)
-
-    if user_response.status_code != 200 or todos_response.status_code != 200:
-        print("Error: Failed to fetch data from the API.")
-        sys.exit(1)
-
-        user_data = user_response.json()
-        todos_data = todos_response.json()
-
-        return user_data, todos_data
-
-
-def export_to_json(user_data, todos_data):
-    """
-    Exports data to JSON format.
-
-    Args:
-        user_data (dict): User data fetched from the API.
-        todos_data (list): TODO list data fetched from the API.
-    """
-    employee_id = user_data['id']
-    employee_name = user_data['name']
-    json_filename = f'{employee_id}.json'
-
-    tasks = [{
-        "task": task['title'],
-        "completed": task['completed'],
-        "username": employee_name
-        }
-        for task in todos_data
-        ]
-    data = {str(employee_id): tasks}
-
-    with open(json_filename, 'w') as jsonfile:
-        json.dump(data, jsonfile, indent=4)
-
-        print("Data exported to {json_filename}")
-
-
-if "__name__" == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 export_to_JSON.py[employee_id]")
-        sys.exit(1)
-
-        try:
-            employee_id = int(sys.argc[1])
-        except ValueError:
-            print("Error: Invalid employee ID. Please provide an integer ID.")
-            sys.exit(1)
-
-    user_data, todos_data = get_employee_info(employee_id)
-    export_to_json(user_data, todos_data)
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
