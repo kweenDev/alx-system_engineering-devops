37b6ce2ee56954cf936ec9532f22ecf99f3d9ae1
kweenDev <refiloe15radebe@gmail.com>
2024-05-17 00:17:50 +0200

    Fix attribute placement error, to improve readability and consistency.

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,19 +1,10 @@
-# Puppet manifest to replace a line in a file using file_line resource
-include stdlib  # Include the stdlib module
+# A puppet manuscript to replace a line in a file on a server
 
-# Define the file path
 $file_to_edit = '/var/www/html/wp-settings.php'
 
-# Ensure the file_line module is available (if not already installed)
-package { 'puppetlabs-stdlib':
-  ensure => installed,
-}
+#replace line containing "phpp" with "php"
 
-# Replace the line containing "phpp" with "php"
-file_line { 'replace_line_in_wp_settings':
-  ensure  => present,               # Ensure is the first attribute
-  path    => $file_to_edit,
-  line    => 'new_line_to_insert',  # Replace this with the actual line you want to insert
-  match   => '^.*phpp.*$',         # Regular expression to match the line containing "phpp"
-  require => File[$file_to_edit],  # Ensure the file exists before modifying it
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
